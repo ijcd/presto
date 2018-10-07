23e71cd9d08d0dd2ce2e07cfbacf6056daed91b5
Ian Duggan <ian@ianduggan.net>
2018-10-07 16:44:35 -0700

    Works with basic ES6 modules.

--- assets/tests/sum.test.js
+++ assets/tests/sum.test.js
@@ -1,5 +1,7 @@
+import sum from '../js/sum.js';
+
 describe('sum', function () {
-    it('should return sum of arguments', function () {
-        chai.expect(sum(1, 2)).to.equal(3);
-    });
+  it('should return sum of arguments', function () {
+    chai.expect(sum(1, 2)).to.equal(3);
+  });
 });
